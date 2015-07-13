22707ecd09a096bdbcb1d27c20fd7ce1982712a0
frank <snowfox@snowfox.com>
2015-07-14 02:09:53 +0800

    add malloc_multi_dimention_array.c

---
+++ PointersOnC/Ch8/malloc_multi_dimention_array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+ * write a matrix allocator, which returns a pointer to a matrix by which the user can access elements in the same way as with a double-dimential array  i.e. ptr[i][j]
+ * The basic idea here is first create an array of rows pointers. And then make each of the pointers point to an array of cols elements.
+ * So the return type of the allocator should be int **. And the total memory used by the matrix would be rows * sizeof (int *) + cols * rows * sizeof (int)
+ */
+int ** alloc_matrix (int rows, int cols)
+{
+    int ** ptr = malloc ( rows * sizeof (int *));
+    
+    int i;
+    for (i = 0; i < rows; i++)
+    {
+        ptr[i] = malloc (cols * sizeof(int));
+    }
+    return ptr;
+}
+
+int
+main ()
+{
+    int ** matrix_ptr = alloc_matrix (3, 5);
+    printf ("sizeof matrix_ptr: %lu\n", sizeof (matrix_ptr));
+    
+    int i, j;
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 5; j++)
+        {
+            matrix_ptr[i][j] = i + j;
+        }
+    printf ("matrix_ptr[2][4]: %d\n", matrix_ptr[2][4]);
+}
